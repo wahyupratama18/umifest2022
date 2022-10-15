d4f24712cf9ebaeb5d6d67e48e321a0ce5093b09
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2022-10-15 10:12:40 +0700

    Summarize

--- js/input.js
+++ js/input.js
@@ -1,14 +1,8 @@
 import Alpine from 'alpinejs';
 import AOS from 'aos';
-// import Scrollbar from 'smooth-scrollbar';
 
 window.Alpine = Alpine;
 window.AOS = AOS;
 
 Alpine.start();
 AOS.init();
-// Scrollbar.init(document.querySelector())
-
-// document.querySelector("navbar a").addEventListener("click", () => {
-//     location.pathname.replace(/^\//, "")
-// })
